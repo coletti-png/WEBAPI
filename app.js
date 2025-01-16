69bd17e0635afeb636f033fd9160805f37a5db53
Coletti-png <tdcoletti@email.neit.edu>
2025-01-16 13:04:18 -0500

    bbbb

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-var msg = "Hello World";
-console.log(msg);
+const express = require("express"); 
+const app = express(); 
+  
+// Root route of express app 
+app.get("/", (req, res) => { 
+  res.send("Hello Geeks"); 
+}); 
+  
+app.get("/new", (req, res) => { 
+  res.send("welcome to new page"); 
+}); 
+  
+// All the general routes of your 
+// web app are defined above the 
+// default route 
+  
+// Default route 
+app.get("*", (req, res) => { 
+  
+  // Here user can also design an 
+  // error page and render it  
+  res.send("PAGE NOT FOUND"); 
+}); 
+  
+// Server setup 
+app.listen(3000, () => { 
+  console.log( 
+`Server listening on http://localhost:3000`); 
+}); 
